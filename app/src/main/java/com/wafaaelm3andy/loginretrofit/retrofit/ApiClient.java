7c61c6868db28a013ba602ce89f99377e7c227ad
Wafaa Elsaeed <wafaa11995@yahoo.com>
2018-07-06 05:37:55 -0700

    addLog

--- app/src/main/java/com/wafaaelm3andy/loginretrofit/retrofit/ApiClient.java
+++ app/src/main/java/com/wafaaelm3andy/loginretrofit/retrofit/ApiClient.java
@@ -1,5 +1,9 @@
 package com.wafaaelm3andy.loginretrofit.retrofit;
 
+import com.wafaaelm3andy.loginretrofit.BuildConfig;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,10 +14,20 @@ public class ApiClient {
 
 
     public static Retrofit getClient() {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+if(BuildConfig.DEBUG){
+httpClient.addInterceptor(logging);
+}
+
+
         if (retrofit==null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build())
                     .build();
         }
         return retrofit;
